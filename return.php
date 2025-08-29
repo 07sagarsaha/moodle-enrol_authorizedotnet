888596ee7b01b7af817dab65b8acac8070ee94f7
Sagar Saha <ss2749968@gmail.com>
2025-08-29 18:48:17 +0530

    ..

--- return.php
+++ return.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once('../../../config.php');
 require_once($CFG->libdir . '/enrollib.php');
 require_once($CFG->dirroot . '/enrol/authorizedotnet/vendor/authorizenet/authorizenet/autoload.php');
@@ -11,6 +10,8 @@ $courseid = required_param('courseid', PARAM_INT);
 $course = $DB->get_record('course', ['id' => $courseid], '*', MUST_EXIST);
 $context = context_course::instance($course->id);
 
+require_login($course);
+
 $PAGE->set_url('/enrol/authorizedotnet/return.php', ['courseid' => $course->id]);
 $PAGE->set_context($context);
 $PAGE->set_title(get_string('paymentverification', 'enrol_authorizedotnet'));
@@ -19,14 +20,13 @@ $PAGE->set_heading($course->fullname);
 echo $OUTPUT->header();
 
 if (!isset($SESSION->enrol_authorizedotnet_invoicenumber) || !isset($SESSION->enrol_authorizedotnet_instanceid)) {
-    // Something is wrong, maybe the user came here directly.
     redirect(new moodle_url('/course/view.php', ['id' => $course->id]));
 }
 
 $invoiceNumber = $SESSION->enrol_authorizedotnet_invoicenumber;
 $instanceid = $SESSION->enrol_authorizedotnet_instanceid;
 
-// Clean up session variables.
+// Clean up session.
 unset($SESSION->enrol_authorizedotnet_invoicenumber);
 unset($SESSION->enrol_authorizedotnet_instanceid);
 
@@ -35,7 +35,7 @@ $merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
 $merchantAuthentication->setName($plugin->get_config('loginid'));
 $merchantAuthentication->setTransactionKey($plugin->get_config('transactionkey'));
 
-// Set up the request to get a list of unsettled transactions.
+// TODO: ideally you should store transactionId in session and call GetTransactionDetailsRequest
 $request = new AnetAPI\GetUnsettledTransactionListRequest();
 $request->setMerchantAuthentication($merchantAuthentication);
 
@@ -46,62 +46,49 @@ $endpoint = $useSandbox ? \net\authorize\api\constants\ANetEnvironment::SANDBOX 
 $response = $controller->executeWithApiResponse($endpoint);
 
 $transactionFound = false;
-if ($response != null && $response->getMessages()->getResultCode() == "Ok") {
-    if ($response->getTransactions() != null) {
-        foreach ($response->getTransactions() as $transaction) {
-            if ($transaction->getInvoiceNumber() == $invoiceNumber) {
-                // Found the transaction. We can assume it was successful because it's in the unsettled list.
-                $transactionFound = true;
-                $instance = $DB->get_record('enrol', ['id' => $instanceid], '*', MUST_EXIST);
-                $user = $DB->get_record('user', ['id' => $USER->id], '*', MUST_EXIST);
+if ($response && $response->getMessages()->getResultCode() == "Ok" && $response->getTransactions() != null) {
+    foreach ($response->getTransactions() as $transaction) {
+        if ($transaction->getInvoiceNumber() == $invoiceNumber) {
+            $transactionFound = true;
+            $instance = $DB->get_record('enrol', ['id' => $instanceid], '*', MUST_EXIST);
+            $user = $DB->get_record('user', ['id' => $USER->id], '*', MUST_EXIST);
 
-                if ($instance->enrolperiod) {
-                    $timestart = time();
-                    $timeend = $timestart + $instance->enrolperiod;
-                } else {
-                    $timestart = 0;
-                    $timeend = 0;
-                }
+            $timestart = time();
+            $timeend = $instance->enrolperiod ? $timestart + $instance->enrolperiod : 0;
 
-                // Enrol the user.
-                $plugin->enrol_user($instance, $user->id, $instance->roleid, $timestart, $timeend);
+            $plugin->enrol_user($instance, $user->id, $instance->roleid, $timestart, $timeend);
 
-                // Send notification.
-                $teacher = get_course_teacher($course->id);
-                $mailstudents = $plugin->get_config('mailstudents');
-                if (!empty($mailstudents)) {
-                    $a = new stdClass();
-                    $a->course = format_string($course->fullname, true, array('context' => $context));
-                    $a->user = fullname($user);
-                    $a->teacher = fullname($teacher);
-                    $a->teacherrole = get_string('teacher');
-                    
-                    $subject = get_string('enrolmentnew', 'enrol', $a->course);
-                    $message = get_string('welcometocoursetext', 'enrol_authorizedotnet', $a);
-                    
-                    $userfrom = empty($teacher) ? core_user::get_noreply_user() : $teacher;
-                    
-                    message_send(new \core\message\message([
-                        'userfrom' => $userfrom,
-                        'userto' => $user,
-                        'subject' => $subject,
-                        'fullmessage' => $message,
-                        'fullmessageformat' => FORMAT_HTML,
-                        'fullmessagehtml' => $message,
-                        'smallmessage' => '',
-                        'notification' => 1,
-                    ]));
-                }
+            // Find a teacher.
+            $teachers = get_role_users($instance->roleid, $context);
+            $teacher = !empty($teachers) ? reset($teachers) : core_user::get_noreply_user();
 
-                redirect(new moodle_url('/course/view.php', ['id' => $course->id]), get_string('paymentthanks', 'enrol_authorizedotnet'), 5);
-                break;
-            }
+            // Notify user.
+            $a = new stdClass();
+            $a->course = format_string($course->fullname, true, ['context' => $context]);
+            $a->user = fullname($user);
+            $a->teacher = fullname($teacher);
+
+            $subject = get_string('enrolmentnew', 'enrol', $a->course);
+            $message = get_string('welcometocoursetext', 'enrol_authorizedotnet', $a);
+
+            message_send(new \core\message\message([
+                'userfrom' => $teacher,
+                'userto' => $user,
+                'subject' => $subject,
+                'fullmessage' => $message,
+                'fullmessageformat' => FORMAT_HTML,
+                'fullmessagehtml' => $message,
+                'smallmessage' => '',
+                'notification' => 1,
+            ]));
+
+            redirect(new moodle_url('/course/view.php', ['id' => $course->id]), get_string('paymentthanks', 'enrol_authorizedotnet'), 5);
+            break;
         }
     }
 }
 
 if (!$transactionFound) {
-    // Transaction not found or API error.
     echo $OUTPUT->notification(get_string('paymentprocessing', 'enrol_authorizedotnet'));
     echo $OUTPUT->continue_button(new moodle_url('/course/view.php', ['id' => $course->id]));
 }
